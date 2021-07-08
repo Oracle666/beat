13516c66fcd528b5d2e1c509bac7969ccf2d7ff9
Oracle666 <82901452+Oracle666@users.noreply.github.com>
2021-07-08 05:02:09 +0300

    Add files via upload

---
+++ src/js/slider.js
@@ -0,0 +1,17 @@
+/*slider*/
+(function(){
+    const slider = $('.assortment__list').bxSlider({
+        pager:false,
+        controls:false
+    });
+    
+    $('.slide__left').click(function(e){
+        e.preventDefault();
+        slider.goToPrevSlide();
+    })
+    $('.slide__right').click(function(e){
+        e.preventDefault();
+        slider.goToNextSlide();
+    })
+})()
+
---
+++ src/js/team.js
@@ -0,0 +1,8 @@
+/*Team*/
+(function(){
+    $('.team-item').on('click', function(e){
+        e.preventDefault();
+        $(this).toggleClass('active').siblings().removeClass('active')
+    })
+})()
+
